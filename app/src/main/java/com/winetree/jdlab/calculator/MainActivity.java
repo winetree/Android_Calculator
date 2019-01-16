46864faae04f2c85bda865f7c4952c6b7ec9447b
winetree <winetree@jongdali.com>
2019-01-16 10:06:31 +0900

    UI업데이트

--- app/src/main/java/com/winetree/jdlab/calculator/MainActivity.java
+++ app/src/main/java/com/winetree/jdlab/calculator/MainActivity.java
@@ -1,217 +1,314 @@
 package com.winetree.jdlab.calculator;
 
-import android.databinding.DataBindingUtil;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import com.winetree.jdlab.calculator.databinding.ActivityMainBinding;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MainPresenter.Views {
+public class MainActivity extends AppCompatActivity implements MainPresenter.Views, NavigationView.OnNavigationItemSelectedListener {
 
-  private MainPresenter mainPresenter;
+    private MainPresenter mainPresenter;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    // 데이터 바인딩
-    final ActivityMainBinding bind = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // 데이터 바인딩
+        setContentView(R.layout.activity_main);
 
-    // Presenter Setup
-    mainPresenter = new MainPresenterImpl(bind);
-    mainPresenter.setViews(this);
+        // Presenter Setup
+        mainPresenter = new MainPresenterImpl(this);
+        mainPresenter.setViews(this);
 
-    List<Button> numberPad = new ArrayList<>();
+        //action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    numberPad.add(bind.button0);
-    numberPad.add(bind.button1);
-    numberPad.add(bind.button2);
-    numberPad.add(bind.button3);
-    numberPad.add(bind.button4);
-    numberPad.add(bind.button5);
-    numberPad.add(bind.button6);
-    numberPad.add(bind.button7);
-    numberPad.add(bind.button8);
-    numberPad.add(bind.button9);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        List<Button> numberPad = new ArrayList<>();
+
+        numberPad.add((Button) findViewById(R.id.button_0));
+        numberPad.add((Button) findViewById(R.id.button_1));
+        numberPad.add((Button) findViewById(R.id.button_2));
+        numberPad.add((Button) findViewById(R.id.button_3));
+        numberPad.add((Button) findViewById(R.id.button_4));
+        numberPad.add((Button) findViewById(R.id.button_5));
+        numberPad.add((Button) findViewById(R.id.button_6));
+        numberPad.add((Button) findViewById(R.id.button_7));
+        numberPad.add((Button) findViewById(R.id.button_8));
+        numberPad.add((Button) findViewById(R.id.button_9));
+
+        TextView resultText = findViewById(R.id.result);
+        TextView operatorText = findViewById(R.id.operator);
+        TextView stateText = findViewById(R.id.state);
+
+        Button plusMinus = findViewById(R.id.button_plus_minus);
+        Button backSpace = findViewById(R.id.button_backspace);
+        Button decimalPoint = findViewById(R.id.button_decimal_point);
+        Button clear = findViewById(R.id.button_clear);
+        Button clearEntry = findViewById(R.id.button_clear_entry);
+        Button division = findViewById(R.id.button_division);
+        Button multiplication = findViewById(R.id.button_multiplication);
+        Button plus = findViewById(R.id.button_plus);
+        Button minus = findViewById(R.id.button_minus);
+        Button calculate = findViewById(R.id.button_calculate);
+        Button percentage = findViewById(R.id.button_percent);
+        Button root = findViewById(R.id.button_root);
+        Button square = findViewById(R.id.button_square);
+        Button division1 = findViewById(R.id.button_division_1);
 
 
-    Button plusMinus = bind.buttonPlusMinus;
-    Button backSpace = bind.buttonBackspace;
-    Button decimalPoint = bind.buttonDecimalPoint;
-    Button clear = bind.buttonClear;
-    Button clearEntry = bind.buttonClearEntry;
-    Button division = bind.buttonDivision;
-    Button multiplication = bind.buttonMultiplication;
-    Button plus = bind.buttonPlus;
-    Button minus = bind.buttonMinus;
-    Button calculate = bind.buttonCalculate;
-    Button percentage = bind.buttonPercent;
-    Button root = bind.buttonRoot;
-    Button square = bind.buttonSquare;
-    Button division1 = bind.buttonDivision1;
 
-    /**
-     * 넘버패드 클릭 이벤트 제어
-     */
-    for (int i = 0; i < numberPad.size(); i++) {
-      numberPad.get(i).setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-          mainPresenter.numberPadHandler(v);
+        /**
+         * 넘버패드 클릭 이벤트 제어
+         */
+        for (int i = 0; i < numberPad.size(); i++) {
+            numberPad.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mainPresenter.numberPadHandler(v);
+                }
+            });
         }
-      });
+
+        /**
+         * 플러스 마이너스 전환 클릭 이벤트 제어
+         */
+        plusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.plusMinus();
+            }
+        });
+
+        /**
+         * 백스페이스 버튼 클릭 이벤트 제어
+         */
+        backSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.backSpace();
+            }
+        });
+
+        /**
+         * 소수점 추가 버튼 클릭 이벤트 제어
+         */
+        decimalPoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.decimalPoint();
+            }
+        });
+
+        /**
+         * 데이터 전체 삭제
+         */
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.clear();
+            }
+        });
+
+        /**
+         * 현재 입력값 삭제
+         */
+        clearEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.clearEntry();
+            }
+        });
+
+        /**
+         * 나누기 버튼 클릭 이벤트 제어
+         */
+        division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.division();
+            }
+        });
+
+        /**
+         * 곱하기
+         */
+        multiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.multiplication();
+            }
+        });
+
+        /**
+         * 더하기
+         */
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.plus();
+            }
+        });
+
+        /**
+         * 빼기
+         */
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.minus();
+            }
+        });
+
+        /**
+         * 계산
+         */
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.calculate();
+            }
+        });
+
+        /**
+         * 퍼센티지
+         */
+        percentage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.percentage();
+            }
+        });
+        /**
+         * 근
+         */
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.root();
+            }
+        });
+        /**
+         * 제곱
+         */
+        square.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.square();
+            }
+        });
+        /**
+         * 백분율
+         */
+        division1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.division1();
+            }
+        });
+
     }
 
     /**
-     * 플러스 마이너스 전환 클릭 이벤트 제어
+     * CALLBACK
      */
-    plusMinus.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mainPresenter.plusMinus();
-      }
-    });
+
+    @Override
+    public void setConfirmText(String text) {
+
+    }
 
     /**
-     * 백스페이스 버튼 클릭 이벤트 제어
+     * Action Bar
      */
-    backSpace.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mainPresenter.backSpace();
-      }
-    });
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-    /**
-     * 소수점 추가 버튼 클릭 이벤트 제어
-     */
-    decimalPoint.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        mainPresenter.decimalPoint();
-      }
-    });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-    /**
-     * 데이터 전체 삭제
-     */
-    clear.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        mainPresenter.clear();
-      }
-    });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    /**
-     * 현재 입력값 삭제
-     */
-    clearEntry.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        mainPresenter.clearEntry();
-      }
-    });
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-    /**
-     * 나누기 버튼 클릭 이벤트 제어
-     */
-    division.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        mainPresenter.division();
-      }
-    });
+        return super.onOptionsItemSelected(item);
+    }
 
-    /**
-     * 곱하기
-     */
-    multiplication.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mainPresenter.multiplication();
-      }
-    });
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-    /**
-     * 더하기
-     */
-    plus.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mainPresenter.plus();
-      }
-    });
+        if (id == R.id.standard) {
 
-    /**
-     * 빼기
-     */
-    minus.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v) {
-        mainPresenter.minus();
-      }
-    });
 
-    /**
-     * 계산
-     */
-    calculate.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        mainPresenter.calculate();
-      }
-    });
+        } else if (id == R.id.nav_gallery) {
 
-    /**
-     * 퍼센티지
-     */
-    percentage.setOnClickListener(new View.OnClickListener(){
-    	@Override
-	    public void onClick(View v){
-    	  mainPresenter.percentage();
-	    }
-    });
-    /**
-     * 근
-     */
-    root.setOnClickListener(new View.OnClickListener(){
-    	@Override
-	    public void onClick(View v){
-    	  mainPresenter.root();
-	    }
-    });
-    /**
-     * 제곱
-     */
-    square.setOnClickListener(new View.OnClickListener(){
-    	@Override
-	    public void onClick(View v){
-    	  mainPresenter.square();
-	    }
-    });
-    /**
-     * 백분율
-     */
-    division1.setOnClickListener(new View.OnClickListener(){
-    	@Override
-	    public void onClick(View v){
-    	  mainPresenter.division();
-	    }
-    });
+        } else if (id == R.id.nav_slideshow) {
 
-  }
+        } else if (id == R.id.nav_manage) {
 
-  /**
-   * CALLBACK
-   */
+        } else if (id == R.id.nav_share) {
 
-  @Override
-  public void setConfirmText(String text) {
+        } else if (id == R.id.nav_send) {
 
-  }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
